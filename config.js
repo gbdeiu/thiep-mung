17dc66c960cfd413294b1bab898ac394eeca6f6f
gbdeiu <105587845+gbdeiu@users.noreply.github.com>
2022-05-15 17:58:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const nameGirl = 'Chị iuu';
+const nameGirl = 'Nhật Hạ';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng 20-10';
-const titleCard = 'Tặng Nguyễn Trần Gia Nghi';
-const contentCard = 'Chúc chị iuu của emm 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với chị trong hôm nay và cả những ngày sau';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng Nhật Hạ nhé<3';
+const contentCard = 'Chúc cậu sinh nhật có nhiều niềm vui tràn ngập niềm vui và những nụ cười, tuổi mới chúc bạn ngày càng trưởng thành và xinh xắn hơn nhé<3';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
